f198071bbd6b3fd257f3c7bd4b4c350aafd9532b
david.campbell <dave_ac99@yahoo.com.au>
2017-12-27 14:58:48 +1100

    Migrated from .net core version 1.0.3 to 2.0.1

--- CycleFinder/Program.cs
+++ CycleFinder/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore;
 
 namespace CycleFinder
 {
@@ -13,16 +14,25 @@ namespace CycleFinder
         public static void Main(string[] args)
         {
 
+            BuildWebHost(args).Run();
 
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseUrls("http://0.0.0.0:5000")   // http://*:1000", "https://*:1234", "http://0.0.0.0:5000")
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
 
-            host.Run();
+
+            //var host = new WebHostBuilder()
+            //    .UseKestrel()
+            //    .UseUrls("http://0.0.0.0:5000")   // http://*:1000", "https://*:1234", "http://0.0.0.0:5000")
+            //    .UseContentRoot(Directory.GetCurrentDirectory())
+            //    .UseIISIntegration()
+            //    .UseStartup<Startup>()
+            //    .Build();
+
+            //host.Run();
         }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>()
+            .Build();
+
     }
 }
